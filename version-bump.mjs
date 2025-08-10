acd29a4903dda3c1931b39ae871e5b22adecc4b5
deuscx <1475453714@qq.com>
2025-08-10 16:09:09 +0800

    chore: update version

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,20 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+import process from 'node:process'
 
-const targetVersion = process.env.npm_package_version;
+const targetVersion = process.env.npm_package_version
+
+function r(p) {
+  return resolve(import.meta.dirname, p)
+}
 
 // read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
-const { minAppVersion } = manifest;
-manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+let manifest = JSON.parse(readFileSync(r('manifest.json'), 'utf8'))
+const { minAppVersion } = manifest
+manifest.version = targetVersion
+writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'))
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+let versions = JSON.parse(readFileSync(r('versions.json'), 'utf8'))
+versions[targetVersion] = minAppVersion
+writeFileSync('versions.json', JSON.stringify(versions, null, '\t'))
