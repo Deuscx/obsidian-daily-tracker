9da84d446d90f7de33b9702af8f66a09edee1ce4
deuscx <1475453714@qq.com>
2025-08-10 15:59:39 +0800

    feat: add line chart for notes

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-# Starter Obsidian Plugin Template
+# Obsidian Daily Tracker
 
-This is a starter template for Obsidian plugin build with Vite.
+This is a Obsidian plugin for tracking daily tasks.
 
+## Usage
+
+```
+
+```dataviewjs
+// Update this object
+const trackerData = {
+    entries: [],
+}
+
+// Path to the folder with notes
+const PATH_TO_YOUR_FOLDER = "jounal/daily";
+const PARAMETER_NAME = 'weight';
+
+// You need dataviewjs plugin to get information from your pages
+for(let page of dv.pages(`"${PATH_TO_YOUR_FOLDER}"`).where((p) => p[PARAMETER_NAME])){
+    trackerData.entries.push({
+        date: page.file.name,
+        intensity: page[PARAMETER_NAME],
+        content: await dv.span(`[](${page.file.name})`)
+    });
+}
+
+renderDailyTracker(this.container, trackerData);
+```
+
+```
+
+## Credit
+
+- https://github.com/mokkiebear/heatmap-tracker
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,59 @@
-import { Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { Chart } from '@antv/g2'
+import { Plugin } from 'obsidian'
 
-export default class ObsidianVite extends Plugin {
-	onload(): Promise<void> | void {
-		new Notice("ObsidianVite plugin loaded");
-		this.addSettingTab(new ObsidianViteSettingTab(this.app, this));
-
-		new Notice("hello world");
-
-	}
-
-	onunload(): void {
-		new Notice("ObsidianVite plugin unloaded");
-	}
+interface Entry {
+  date: string
+  intensity: number
 }
 
-class ObsidianViteSettingTab extends PluginSettingTab {
-	display(): void {
-		const { containerEl } = this;
-		containerEl.empty();
+function parseEntries(entries: Entry[]) {
+  return entries.map((entry) => {
+    return {
+      date: entry.date,
+      value: Number(entry.intensity),
+    }
+  })
+}
 
-		new Setting(containerEl)
-			.setName("A sample setting")
-			.setDesc("some description.")
-			.addText((cmp) =>
-				cmp
-					.setPlaceholder("placeholder text")
-					.onChange((v) => console.log("sample setting changed: ", v))
-			);
-	}
+function renderDailyTracker(container: HTMLElement, config: any) {
+  container.style.height = '400px'
+  const data = parseEntries(config.entries)
+  const chart = new Chart({
+    container,
+    autoFit: true,
+  })
+
+  chart
+    .data(data)
+    .encode('x', 'date')
+    .encode('y', 'value')
+    .scale('x', {
+      range: [0, 1],
+    })
+    .scale('y', {
+      domainMin: 0,
+      nice: true,
+    })
+
+  chart.line().label({
+    text: 'value',
+    style: {
+      dx: -10,
+      dy: -12,
+    },
+  })
+
+  chart.point().style('fill', 'white').tooltip(false)
+
+  chart.render()
+}
+export default class ObsidianDailyTracker extends Plugin {
+  onload(): Promise<void> | void {
+    // eslint-disable-next-line ts/ban-ts-comment
+    // @ts-ignore
+    window.renderDailyTracker = renderDailyTracker
+  }
+
+  onunload(): void {
+  }
 }
